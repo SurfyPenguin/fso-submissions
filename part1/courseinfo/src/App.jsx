78d8594cffb37a795f60626b1b38da1f5d62afcb
SurfyPenguin <186186766+SurfyPenguin@users.noreply.github.com>
2026-04-13 23:03:17 +0530

    exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,10 +6,20 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <div>
       {props.part} {props.exercises}
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part[0]} exercises={props.exercises[0]}/>
+      <Part part={props.part[1]} exercises={props.exercises[1]}/>
+      <Part part={props.part[2]} exercises={props.exercises[2]}/>
     </div>
   )
 }
@@ -35,9 +45,7 @@ const App = () => {
   return (
     <div>
     <Header course={course} />
-    <Content part={part1} exercises={exercises1}/>
-    <Content part={part2} exercises={exercises2}/>
-    <Content part={part3} exercises={exercises3}/>
+    <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]}/>
     <Total exercises={[exercises1, exercises2, exercises3]}/>
     </div>
   )
