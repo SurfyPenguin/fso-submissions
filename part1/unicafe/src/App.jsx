4670e2e5b07de4f606ff589a68f1b8a7c47125f4
SurfyPenguin <186186766+SurfyPenguin@users.noreply.github.com>
2026-06-24 21:16:58 +0530

    feat: update exercise 1.10

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+
+const StatisticLine = ({ text, value }) => <p>{text} {value}</p>
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  if (total  === 0) {
+    return (
+      <p>no feedback given</p>
+    )
+  }
+  
+  const average = (good - bad) / total
+  return (
+    <div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="total" value={total} />
+      <StatisticLine text="average" value={average} />
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+  }
+
+  const handleNeutral = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+  }
+
+  const handleBad = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+  }
+
+  return (
+    <div>
+      <h2>give feedback here</h2>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h2>statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
